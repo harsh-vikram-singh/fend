b070a8d710ae997db5691f4800c7bd131bc881fa
harsh-vikram-singh <singh.harshvikram@gmail.com>
2020-05-21 01:57:02 +0530

    started the capstone project using the scaffold folder

---
+++ capstone/src/client/index.js
@@ -0,0 +1,5 @@
+import './styles/main.scss'
+import './styles/_landingPage.scss'
+import { MDCTextField } from '@material/textfield';
+
+console.log('js attached successfully...')
---
+++ capstone/webpack.prod.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "images"
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+    ]
+}
