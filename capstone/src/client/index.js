6ca2709151a549a792196ea5687976d0ab1ee64e
harsh-vikram-singh <singh.harshvikram@gmail.com>
2020-05-23 00:12:57 +0530

    completed the front end for the first desktop page. form data capturing functionality completed

--- capstone/src/client/index.js
+++ capstone/src/client/index.js
@@ -1,7 +1,29 @@
 import './styles/main.scss'
 import './styles/_landingPage.scss'
-import { MDCTextField } from '@material/textfield';
-import { MDCRipple } from '@material/ripple/index';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
-console.log('js attached successfully...')
+
+const destination = document.querySelector('#destination');
+const travelDate = document.querySelector('#travelDate');
+
+// setting the default value of date to present date
+const date = new Date();
+const month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
+const dateValue = `${date.getFullYear()}-${month}-${date.getDate()}`;
+travelDate.value = dateValue;
+
+const submit = document.querySelector('#submit-btn');
+
+let formData = {}
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  formData = {
+    destination: destination.value,
+    travelDate: travelDate.value
+  }
+  destination.value = "";
+  travelDate.value = "";
+  // console.log(e);
+  console.log('submit button clicked')
+  console.log(formData)
+});
